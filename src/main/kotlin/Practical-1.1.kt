6115a89ceb78e7fdaa49328e42126ee7a7ca87cf
Jimil Patel <jimilkumarpatel21@gnu.ac.in>
2023-08-13 20:48:02 +0530

    Practical-1 done

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,23 @@
+fun main(){
+    //println("hello world")
+    var integer:Int=22;
+    var float:Float=1.5f;
+    var character:Char='T';
+    var strig:String="Jimil";
+    var boolean:Boolean=true;
+    var double:Double=84.69;
+    var long:Long=58763557;
+    var short:Short=-2;
+    var byte:Byte=126;
+
+    println("Integer Value : $integer");
+    println("Float Value : $float");
+    println("Character Value : $character");
+    println("String Value : $strig");
+    println("Boolean Value : $boolean");
+    println("Double Value : $double");
+    println("Long Value : $long");
+    println("Short Value : $short");
+    println("Byte Value : $byte");
+
+}
